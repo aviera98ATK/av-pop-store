f0ed783599fe7eef9d85026729d1280a16922a83
Alejandro Viera <aviera@australtek.com>
2021-05-30 15:01:45 -0300

    Added E-commerce menu using materialize.

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+function NavBar() {
+
+    return(
+        <nav>
+            <div class="nav-fixed">
+            <a href="#" class="brand-logo">AV PopStore</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Contacto</a></li>
+            </ul>
+            </div>
+        </nav>
+    )
+
+}
+
+export default NavBar;
